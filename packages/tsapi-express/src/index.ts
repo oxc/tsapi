4825ef121211ac0c28725eed181144cc9e5c1c3d
Bernhard Frauendienst <git@nospam.obeliks.de>
2023-07-09 01:33:24 +0200

    feat(express): support adding (custom) middleware

--- packages/tsapi-express/src/index.ts
+++ packages/tsapi-express/src/index.ts
@@ -39,13 +39,21 @@ export type EndpointRequest<E extends ApiEndpoint<any>> = Request<
 >;
 
 export type EndpointResponse<E extends ApiEndpoint<any>> = Response<inferEndpointOutputInput<E>>;
-export type EndpointHandler<E extends ApiEndpoint<any>> = (
-  args: EndpointArgs<E>,
+export type EndpointHandler<E extends ApiEndpoint<any>, ExtraArgs extends Record<string, any>> = (
+  args: EndpointArgs<E> & ExtraArgs,
 ) => PromiseLike<inferEndpointOutputInput<E>>;
 
-function createEndpointHandler<Endpoint extends ApiEndpoint<any>>(
+export type Middleware<ExtraArgs extends Record<string, any>> = (
+  args: EndpointArgs<any>,
+) => PromiseLike<ExtraArgs>;
+
+function createEndpointHandler<
+  Endpoint extends ApiEndpoint<any>,
+  ExtraArgs extends Record<string, any>,
+>(
   endpoint: Endpoint,
-  handler: EndpointHandler<Endpoint>,
+  handler: EndpointHandler<Endpoint, ExtraArgs>,
+  middleware: Middleware<ExtraArgs> | undefined,
 ): RequestHandler<
   inferEndpointParamsOutput<Endpoint>,
   inferEndpointOutputInput<Endpoint>,
@@ -64,7 +72,11 @@ function createEndpointHandler<Endpoint extends ApiEndpoint<any>>(
       req.params = args.params;
       req.body = args.body;
       req.query = args.query;
-      handler(args).then(
+      if (middleware) {
+        const extraArgs = await middleware(args);
+        Object.assign(args, extraArgs);
+      }
+      handler(args as EndpointArgs<any> & ExtraArgs).then(
         (output) => {
           res.json(output);
         },
@@ -95,23 +107,35 @@ type ImplementedApiWithRoute<
 > = FlatApiWithRoute<ImplementedApi, Path, inferFlatApi<inferRoute<Api, Path>>>;
 
 type EmptyImplementedApi = EmptyApiType;
-type Builder<Api extends ApiType, FlatApi extends ApiType, ImplementedApi extends ApiType> = (
-  router: ApiRouter<Api, FlatApi, EmptyImplementedApi>,
-) => ApiRouter<Api, FlatApi, ImplementedApi>;
+type Builder<
+  Api extends ApiType,
+  FlatApi extends ApiType,
+  ImplementedApi extends ApiType,
+  ExtraArgs extends Record<string, any>,
+> = (
+  router: ApiRouter<Api, FlatApi, EmptyImplementedApi, ExtraArgs>,
+) => ApiRouter<Api, FlatApi, ImplementedApi, ExtraArgs>;
 type BuilderOrRouter<
   Api extends ApiType,
   FlatApi extends ApiType,
   ImplementedApi extends ApiType,
-> = ApiRouter<Api, FlatApi, ImplementedApi> | Builder<Api, FlatApi, ImplementedApi>;
+  ExtraArgs extends Record<string, any>,
+> =
+  | ApiRouter<Api, FlatApi, ImplementedApi, ExtraArgs>
+  | Builder<Api, FlatApi, ImplementedApi, ExtraArgs>;
 
 export class ApiRouter<
   Api extends ApiType,
   FlatApi extends ApiType,
   ImplementedApi extends ApiType,
+  ExtraArgs extends Record<string, any>,
 > {
   private readonly actions: ((router: IRouter) => void)[] = [];
 
-  constructor(readonly def: ApiDefinition<Api, FlatApi>) {}
+  constructor(
+    readonly def: ApiDefinition<Api, FlatApi>,
+    private readonly middleware?: Middleware<ExtraArgs>,
+  ) {}
 
   get = this.endpointFactory("get");
 
@@ -127,15 +151,20 @@ export class ApiRouter<
     method: Method,
   ): <Path extends ApiEndpointKeys<FlatApi, Method>>(
     path: Path,
-    handler: EndpointHandler<inferEndpoint<FlatApi, Path, Method>>,
-  ) => ApiRouter<Api, FlatApi, ImplementedApiWithEndpoint<ImplementedApi, FlatApi, Path, Method>> {
+    handler: EndpointHandler<inferEndpoint<FlatApi, Path, Method>, ExtraArgs>,
+  ) => ApiRouter<
+    Api,
+    FlatApi,
+    ImplementedApiWithEndpoint<ImplementedApi, FlatApi, Path, Method>,
+    ExtraArgs
+  > {
     return (path, handler) => {
       const endpoint = this.def.flatApi.endpoints[path][method] as inferEndpoint<
         FlatApi,
         typeof path,
         Method
       >;
-      const requestHandler = createEndpointHandler(endpoint, handler);
+      const requestHandler = createEndpointHandler(endpoint, handler, this.middleware);
       this.actions.push((router) => router[method](path, requestHandler));
       return this;
     };
@@ -146,13 +175,14 @@ export class ApiRouter<
     builderOrORouter: BuilderOrRouter<
       inferApi<inferRoute<Api, Path>>,
       inferFlatApi<inferRoute<Api, Path>>,
-      ImplementedApiWithRoute<ImplementedApi, Api, Path>
+      ImplementedApiWithRoute<ImplementedApi, Api, Path>,
+      ExtraArgs
     >,
-  ): ApiRouter<Api, FlatApi, ImplementedApiWithRoute<ImplementedApi, Api, Path>> {
+  ): ApiRouter<Api, FlatApi, ImplementedApiWithRoute<ImplementedApi, Api, Path>, ExtraArgs> {
     const api = this.def.api.routes[path] as inferRoute<Api, Path>;
     const apiRouter =
       typeof builderOrORouter === "function"
-        ? builderOrORouter(new ApiRouter(api))
+        ? builderOrORouter(new ApiRouter(api, this.middleware))
         : builderOrORouter;
     this.actions.push((router) => router.use(path, apiRouter.applyTo(Router())));
     return this;
@@ -170,7 +200,39 @@ export class ApiRouter<
 
 export function implementApi<Def extends ApiDefinition<any, any>>(
   api: Def,
-  builder: Builder<inferApi<Def>, inferFlatApi<Def>, inferFlatApi<Def>>,
-): ApiRouter<inferApi<Def>, inferFlatApi<Def>, inferFlatApi<Def>> {
-  return builder(new ApiRouter(api));
+  builder: Builder<inferApi<Def>, inferFlatApi<Def>, inferFlatApi<Def>, {}>,
+): ApiRouter<inferApi<Def>, inferFlatApi<Def>, inferFlatApi<Def>, {}>;
+export function implementApi<
+  Def extends ApiDefinition<any, any>,
+  ExtraArgs extends Record<string, any>,
+>(
+  api: Def,
+  middleware: Middleware<ExtraArgs>,
+  builder: Builder<inferApi<Def>, inferFlatApi<Def>, inferFlatApi<Def>, ExtraArgs>,
+): ApiRouter<inferApi<Def>, inferFlatApi<Def>, inferFlatApi<Def>, ExtraArgs>;
+export function implementApi<
+  Def extends ApiDefinition<any, any>,
+  ExtraArgs extends Record<string, any>,
+>(
+  api: Def,
+  middlewareOrBuilder:
+    | Middleware<ExtraArgs>
+    | Builder<inferApi<Def>, inferFlatApi<Def>, inferFlatApi<Def>, ExtraArgs>,
+  builderOrUndefined?: Builder<inferApi<Def>, inferFlatApi<Def>, inferFlatApi<Def>, ExtraArgs>,
+): ApiRouter<inferApi<Def>, inferFlatApi<Def>, inferFlatApi<Def>, ExtraArgs> {
+  let middleware: Middleware<ExtraArgs> | undefined;
+  let builder: Builder<inferApi<Def>, inferFlatApi<Def>, inferFlatApi<Def>, ExtraArgs>;
+  if (!builderOrUndefined) {
+    builder = middlewareOrBuilder as Builder<
+      inferApi<Def>,
+      inferFlatApi<Def>,
+      inferFlatApi<Def>,
+      ExtraArgs
+    >;
+    middleware = undefined;
+  } else {
+    builder = builderOrUndefined;
+    middleware = middlewareOrBuilder as Middleware<ExtraArgs>;
+  }
+  return builder(new ApiRouter(api, middleware));
 }
