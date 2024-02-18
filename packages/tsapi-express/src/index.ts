d49bb5ab2888d377bd3c4d4ee974ea725d14dffb
Bernhard Frauendienst <git@nospam.obeliks.de>
2024-02-18 17:12:10 +0100

    chore(express): move middleware to its own file

--- packages/tsapi-express/src/index.ts
+++ packages/tsapi-express/src/index.ts
@@ -22,8 +22,11 @@ import {
   inferEndpointQueryOutput,
 } from "@oxc/tsapi-core/infer";
 import { IRouter, Request, RequestHandler, Response, Router } from "express";
+import { Middleware } from "./middleware.js";
 
-type EndpointArgs<E extends ApiEndpoint<any>> = {
+export { middleware, Middleware } from "./middleware.js";
+
+export type EndpointArgs<E extends ApiEndpoint<any>> = {
   req: EndpointRequest<E>;
   res: EndpointResponse<E>;
   params: inferEndpointParamsOutput<E>;
@@ -42,10 +45,6 @@ export type EndpointResponse<E extends ApiEndpoint<any>> = Response<inferEndpoin
 export type EndpointHandler<E extends ApiEndpoint<any>, ExtraArgs extends Record<string, any>> = (
   args: EndpointArgs<E> & ExtraArgs,
 ) => PromiseLike<inferEndpointOutputInput<E>>;
-
-export type Middleware<ExtraArgs extends Record<string, any>> = (
-  args: EndpointArgs<any>,
-) => PromiseLike<ExtraArgs>;
 
 function createEndpointHandler<
   Endpoint extends ApiEndpoint<any>,
@@ -237,23 +236,3 @@ export function implementApi<
   return builder(new ApiRouter(api, middleware));
 }
 
-export function middleware<A1 extends Record<string, any>, A2 extends Record<string, any>, A3 extends Record<string, any>, A4 extends Record<string, any>>(
-  middleware1: Middleware<A1>,
-  middleware2: Middleware<A2>,
-  middleware3: Middleware<A3>,
-  middleware4: Middleware<A4>,
-): Middleware<A1 & A2 & A3 & A4>;
-export function middleware<A1 extends Record<string, any>, A2 extends Record<string, any>, A3 extends Record<string, any>>(
-  middleware1: Middleware<A1>,
-  middleware2: Middleware<A2>,
-  middleware3: Middleware<A3>,
-): Middleware<A1 & A2 & A3>;
-export function middleware<A1 extends Record<string, any>, A2 extends Record<string, any>>(
-  middleware1: Middleware<A1>,
-  middleware2: Middleware<A2>,
-): Middleware<A1 & A2>;
-export function middleware(...middlewares: Middleware<any>[]): Middleware<any> {
-  return async (...args) => Object.assign({}, ...(await Promise.all(
-    middlewares.map((middleware) => middleware(...args)),
-  )));
-}
